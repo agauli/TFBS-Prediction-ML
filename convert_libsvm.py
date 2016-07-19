508ec1a62d819914a748b55b646303e5c6f8f132
scottdet <scottdetaboada@g.ucla.edu>
2016-07-19 12:20:20 -0700

    Update convert_libsvm.py

--- convert_libsvm.py
+++ convert_libsvm.py
@@ -1,5 +1,5 @@
 chr_feature_binding= open(str(sys.argv[1]), 'r')
-chr_features_libsvm = open("%s" % str(sys.argv[1]).strip(".tsvfinal")\
+chr_features_libsvm = open("%s" % str(sys.argv[1]).strip(".tsvf")\
                                 + "_libsvm.bz2", 'w')
 chr_feature_binding.readline()
 for line in chr_sequence_binding:
@@ -15,8 +15,9 @@ for line in chr_sequence_binding:
 		string += "2"
 
 	string += " "
-	for num in range(4:4100):
+	for num in range(4, 4100):
 		string += splitline[num]
 
 	chr_features_libsvm.write(string)
 
+chr_features_libsvm.close()
