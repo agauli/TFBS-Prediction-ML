28f805ba39e294b729ad6061bf3389c4b1a4070f
agauli <app.ashu@gmail.com>
2016-07-19 13:29:31 -0700

    edited

--- convert_libsvm.py
+++ convert_libsvm.py
@@ -1,22 +1,24 @@
-chr_feature_binding= open(str(sys.argv[1]), 'r')
-chr_features_libsvm = open("%s" % str(sys.argv[1]).strip(".tsvfinal")\
-                                + "_libsvm.bz2", 'w')
-chr_feature_binding.readline()
+import sys
+chr_sequence_binding= open(str(sys.argv[1]), 'r')
+chr_features_libsvm = open("%s" % str(sys.argv[1])[:-9]+ "_libsvm.bz2", 'w')
+chr_sequence_binding.readline()
 for line in chr_sequence_binding:
-	line = line.rstrip("\n")
-	splitline = line.split("\t")
-	string = ""
-	binding = splitline[4101]
-	if binding == "U":
-		string += "0"
-	elif binding == "B":
-		string += "1"
-	else:
-		string += "2"
+        line = line.rstrip("\n")
+        splitline = line.split("\t")
+        string = ""
+        binding = splitline[4101]
+        if binding == "U":
+                string += "0"
+        elif binding == "B":
+                string += "1"
+        else:
+                string += "2"
 
-	string += " "
-	for num in range(4:4100):
-		string += splitline[num]
+        string += " "
+        for num in range(4, 4100):
+                string += splitline[num]
+        string +="\n"
+        chr_features_libsvm.write(string)
+chr_features_libsvm.close()
 
-	chr_features_libsvm.write(string)
 
